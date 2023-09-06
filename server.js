02828aa9eed3b27196d6244bf08ea02536c4c1d0
Jason <jasontorrealba199@gmail.com>
2023-09-06 12:46:40 -0400

    Created and imported routes to call server

---
+++ server.js
@@ -0,0 +1,50 @@
+// Require all packages/files
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+require('dotenv').config();
+
+// Middleware
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.DB_SESSION_SECRET,
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+// Call server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Server listening on: http://localhost:' + PORT));
+});
