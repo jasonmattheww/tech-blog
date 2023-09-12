5f885257f8e7fa80ec58b78ae1b2789956a08c7b
Jason <jasontorrealba199@gmail.com>
2023-09-12 13:03:49 -0400

    Created add post form handler

---
+++ public/js/addPost.js
@@ -0,0 +1,22 @@
+const addpostFormHandler = async (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value.trim();
+  const content = document.querySelector('#content').value.trim();
+
+  if (title && content) {
+    const response = await fetch('/api/post', {
+      method: 'POST',
+      body: JSON.stringify({ title, content }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert('Failed to add post');
+    }
+  }
+};
+
+document.querySelector('#add-post').addEventListener('submit', addpostFormHandler);
